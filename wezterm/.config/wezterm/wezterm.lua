955c2f3e121aa57c03d085983ea1ed4eab4d2f15
Vicente Aguilera <vicente.aguilerae@gmail.com>
2026-03-19 22:05:34 -0300

    Change window decoration for wezterm window

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 15
 
 config.enable_tab_bar = false
-config.window_decorations = "TITLE | RESIZE"
+config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
