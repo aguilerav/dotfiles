fca033e1297339f4e9cfb2160cbd84198c336986
Vicente Aguilera <vicente.aguilerae@gmail.com>
2026-08-24 00:13:58 -0400

    style(nvim): change lualine design

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,37 +1,6 @@
 return {
 
    -- ========================================================================
-  -- BARRA DE ESTADO INFERIOR (Lualine)
-  -- ========================================================================
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    opts = function()
-      local lazy_status = require("lazy.status")
-      return {
-        options = {
-          theme = "tokyonight",
-          component_separators = "|",
-          section_separators = "",
-        },
-        sections = {
-          lualine_x = {
-            {
-              lazy_status.updates,
-              cond = lazy_status.has_updates,
-              color = { fg = "#ff9e64" },
-            },
-            { "encoding" },
-            { "fileformat" },
-            { "filetype" },
-          },
-        },
-      }
-    end,
-  },
-
-  -- ========================================================================
   -- BARRA DE PESTAÑAS SUPERIOR (Bufferline)
   -- ========================================================================
   {
@@ -41,8 +10,6 @@ return {
     event = "VeryLazy",
     opts = {
       options = {
-        -- Nota: Usabas 'tabs'. Lo moderno suele ser 'buffers', pero respeto tu config.
-        -- Si quieres ver todos los archivos abiertos arriba, cambia esto a 'buffers'.
         mode = "tabs", 
         separator_style = "thin",
       },
@@ -58,7 +25,6 @@ return {
     main = "ibl",
     opts = {
       indent = { char = "┊" },
-      scope = { enabled = false }, -- Desactiva scope para que sea más limpio (opcional)
     },
   }
 }
